108d4d96a93add55362ee6c31abb0e4ce7daff3b
Matheus Facin <matheus.facin@outlook.com>
2020-10-15 14:15:44 -0300

    Third class - Finishing Front End in React

---
+++ web/src/pages/CreateOrphanage.tsx
@@ -0,0 +1,262 @@
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+import { Map, Marker, TileLayer } from 'react-leaflet'
+import { LeafletMouseEvent } from 'leaflet'
+import { FiPlus, FiX } from 'react-icons/fi'
+
+import Sidebar from '../components/Sidebar'
+import mapIcon from '../utils/mapIcon'
+
+import '../styles/pages/create-orphanage.css'
+import api from '../services/api'
+import { AxiosError } from 'axios'
+import { useHistory } from 'react-router-dom'
+
+interface ErrorResponseData {
+  message: string
+  errors: Array<{
+    [key: string]: string[]
+  }>
+}
+
+export default function CreateOrphanage() {
+  const history = useHistory()
+
+  const [userPosition, setUserPosition] = useState<[number, number]>([0, 0])
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 })
+
+  const [name, setName] = useState('')
+  const [about, setAbout] = useState('')
+  const [instructions, setInstructions] = useState('')
+  const [openingHours, setOpeningHours] = useState('')
+  const [openOnWeekends, setOpenOnWeekends] = useState(true)
+  const [images, setImages] = useState<File[]>([])
+
+  const [previewImages, setPreviewImages] = useState<string[]>([])
+
+  // get user position
+  useEffect(() => {
+    ;(() => {
+      navigator.geolocation &&
+        navigator.geolocation.getCurrentPosition((position) => {
+          setUserPosition([position.coords.latitude, position.coords.longitude])
+          console.log(position)
+        })
+    })()
+  }, [])
+
+  function handleMapClick(e: LeafletMouseEvent) {
+    const { lat, lng } = e.latlng
+
+    setPosition({
+      latitude: lat,
+      longitude: lng,
+    })
+  }
+
+  function handleSelectedImages(e: ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files) return
+
+    const selectedImages = Array.from(e.target.files)
+
+    const allImages = [...images, ...selectedImages]
+
+    setImages(allImages)
+
+    const selectedImagesPreview = allImages.map((image) => {
+      return URL.createObjectURL(image)
+    })
+
+    setPreviewImages(selectedImagesPreview)
+  }
+
+  function handleDeleteImage(index: number) {
+    const allImages = Array.from(images)
+
+    allImages.splice(index, 1)
+
+    setImages(allImages)
+
+    const selectedImagesPreview = Array.from(previewImages)
+    selectedImagesPreview.splice(index, 1)
+
+    setPreviewImages(selectedImagesPreview)
+  }
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault()
+
+    const { latitude, longitude } = position
+
+    const data = new FormData()
+
+    data.append('name', name)
+    data.append('latitude', String(latitude))
+    data.append('longitude', String(longitude))
+    data.append('about', about)
+    data.append('instructions', instructions)
+    data.append('opening_hours', openingHours)
+    data.append('open_on_weekends', String(openOnWeekends))
+
+    images.forEach((image) => {
+      data.append('images', image)
+    })
+
+    try {
+      await api.post('orphanages', data)
+      alert('Cadastro realizado com sucesso')
+      history.push('/app')
+    } catch (e) {
+      console.log(e.response)
+      const { errors } = e.response.data as ErrorResponseData
+      let message = 'Não foi possível realizar o cadastro:\n'
+  
+      console.log(errors)
+      console.log(typeof errors)
+
+      for (let error in errors) {
+        message += `\n${errors[error][0]}`
+      }
+  
+      alert(message)
+    }
+  }
+
+  return (
+    <div id="page-create-orphanage">
+      <Sidebar />
+
+      <main>
+        <form className="create-orphanage-form" onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Dados</legend>
+
+            <Map
+              center={userPosition}
+              style={{ width: '100%', height: 280 }}
+              zoom={15}
+              onclick={handleMapClick}
+            >
+              <TileLayer
+                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+              />
+
+              {position.latitude !== 0 && position.longitude !== 0 && (
+                <Marker
+                  interactive={false}
+                  icon={mapIcon}
+                  position={[position.latitude, position.longitude]}
+                />
+              )}
+            </Map>
+
+            <div className="input-block">
+              <label htmlFor="name">Nome</label>
+              <input
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="about">
+                Sobre <span>Máximo de 300 caracteres</span>
+              </label>
+              <textarea
+                id="name"
+                maxLength={300}
+                value={about}
+                onChange={(e) => setAbout(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="images">Fotos</label>
+
+              <div className="images-container">
+                {previewImages.map((image, index) => (
+                  <div key={image} className="image">
+                    <img src={image} alt={name} />
+                    <button
+                      type="button"
+                      onClick={() => handleDeleteImage(index)}
+                    >
+                      <FiX size={30} color="#ff669d" />
+                    </button>
+                  </div>
+                ))}
+
+                <label htmlFor="image[]" className="new-image">
+                  <FiPlus size={24} color="#15b6d6" />
+                </label>
+              </div>
+              <input
+                multiple
+                onChange={handleSelectedImages}
+                type="file"
+                id="image[]"
+              />
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <legend>Visitação</legend>
+
+            <div className="input-block">
+              <label htmlFor="instructions">Instruções</label>
+              <textarea
+                id="instructions"
+                value={instructions}
+                onChange={(e) => setInstructions(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="opening_hours">Horário de funcionamento</label>
+              <input
+                id="opening_hours"
+                value={openingHours}
+                onChange={(e) => setOpeningHours(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="open_on_weekends">Atende fim de semana</label>
+
+              <div className="button-select">
+                <button
+                  type="button"
+                  className={openOnWeekends ? 'active' : ''}
+                  onClick={() => setOpenOnWeekends(true)}
+                >
+                  Sim
+                </button>
+                <button
+                  type="button"
+                  className={
+                    !openOnWeekends
+                      ? 'active dont-open-on-weekends'
+                      : 'dont-open-on-weekends'
+                  }
+                  onClick={() => setOpenOnWeekends(false)}
+                >
+                  Não
+                </button>
+              </div>
+            </div>
+          </fieldset>
+
+          <button className="confirm-button" type="submit">
+            Confirmar
+          </button>
+        </form>
+      </main>
+    </div>
+  )
+}
+
+// return `https://a.tile.openstreetmap.org/${z}/${x}/${y}.png`;
--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,25 +1,49 @@
 import React, { useEffect, useState } from 'react'
-import { FiPlus } from 'react-icons/fi'
+import { FiArrowRight, FiPlus } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
-import { Map, TileLayer } from 'react-leaflet'
-
-import 'leaflet/dist/leaflet.css'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import Leaflet from 'leaflet'
 
 import mapMarkerImg from '../assets/map-marker.svg'
+import api from '../services/api'
 
 import '../styles/pages/orphanages-map.css'
 
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [48, 58],
+  iconAnchor: [24, 58],
+  popupAnchor: [175, 10],
+})
+
+interface Orphanage {
+  id: number
+  name: string
+  latitude: number
+  longitude: number
+}
+
 function OrphanagesMap() {
   const [pos, setPos] = useState<[number, number]>([0, 0])
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([])
 
+  // get orphanages
+  useEffect(() => {
+    ;(async () => {
+      const response = await api.get('/orphanages')
+      const orphs = response.data as Orphanage[]
+      setOrphanages(orphs)
+    })()
+  }, [])
+
+  // get position
   useEffect(() => {
     ;(() => {
-      if (navigator.geolocation) {
+      navigator.geolocation &&
         navigator.geolocation.getCurrentPosition((position) => {
-          console.log(position)
           setPos([position.coords.latitude, position.coords.longitude])
+          console.log(position)
         })
-      }
     })()
   }, [])
 
@@ -41,18 +65,33 @@ function OrphanagesMap() {
         </footer>
       </aside>
 
-      <Map
-        center={pos}
-        zoom={17}
-        style={{ width: '100%', height: '100%' }}
-      >
+      <Map center={pos} zoom={16} style={{ width: '100%', height: '100%' }}>
         {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+        {orphanages.map((orphanage) => (
+          <Marker
+            key={orphanage.id}
+            icon={mapIcon}
+            position={[orphanage.latitude, orphanage.longitude]}
+          >
+            <Popup
+              closeButton={false}
+              minWidth={240}
+              maxWidth={240}
+              className="map-popup"
+            >
+              {orphanage.name}
+              <Link to={`/orphanages/${orphanage.id}`}>
+                <FiArrowRight size={20} color="#fff" />
+              </Link>
+            </Popup>
+          </Marker>
+        ))}
       </Map>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
       </Link>
     </div>
